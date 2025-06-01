e3da32ebbfa09570b186b5eb1ef0d939f4e0c4a1
marcuma <marcuma@gmail.com>
2025-06-01 16:31:25 -0400

    Add Structure

---
+++ cab-pro-api/src/main/java/com/simplilearn/cabproapi/CabProApiApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.cabproapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabProApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabProApiApplication.class, args);
+    }
+
+}
---
+++ cab-pro-api/src/test/java/com/simplilearn/cabproapi/CabProApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.cabproapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabProApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
